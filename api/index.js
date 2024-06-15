ae3865eedb33e62e0e8af7b2bf4adb4bdc2bed92
Rohit yadav <110273052+Rohitkaz@users.noreply.github.com>
2024-06-15 13:10:31 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-webapp-with-database-and-authentication-okbp.vercel.app/",
     credentials: true,
   })
 );
